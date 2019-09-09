c45a9ad174ce1b1af4b19f4e8d85b039684aabbd
Faye Skeen <fskeen@gmail.com>
2019-09-09 13:50:17 -0700

    Basic GET for all accounts working.

--- accounts/accountsRouter.js
+++ accounts/accountsRouter.js
@@ -4,6 +4,29 @@ const db = require('./accountsDb')
 
 router.use(express.json())
 
+// GET all accounts 
 
+router.get('/', async (req, res) => {
+    try {
+        const accounts = await db.get()
+        res.status(200).json(accounts)
+    } catch (err) {
+        console.log(err)
+        res.status(500).json({error: "Error retrieving accounts."})
+    }
+})
+
+// Get account by ID
+
+// router.get('/:id', async (req, res) => {
+//     const id = req.params.id
+//     try {
+//         const account = db.getById(id)
+//         res.status(200).json(account)
+//     } catch (err) {
+//         console.log(err)
+//         res.status(500)
+//     }
+// })
 
 module.exports = router
