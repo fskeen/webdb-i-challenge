635bd5896abfc1b507c10826775f88706a363b00
Faye Skeen <fskeen@gmail.com>
2019-09-09 13:41:15 -0700

    Added routing. Created separate account db file with initial db functions to import into router

---
+++ accounts/accountsDb.js
@@ -0,0 +1,44 @@
+const db = require("../data/dbConfig")
+
+module.exports = {
+    get,
+    getById,
+    insert,
+    update,
+    remove,
+    getByQuery
+}
+
+function get() {
+    return db('accounts');
+}
+
+function getById(id) {
+    return db('accounts')
+        .where('id', id)
+        .first();
+}
+
+function insert(account) {
+    return db('accounts')
+        .insert(account)
+        .then(id => {
+            return getById(id[0])
+        });
+}
+
+function update(id, changes) {
+    return db('accounts')
+        .where('id', id)
+        .update(changes);
+}
+
+function remove(id) {
+    return db('accounts')
+        .where('id', id)
+        .del();
+}
+
+function getByQuery() {
+    console.log("Figure this out in a bit!")
+}
